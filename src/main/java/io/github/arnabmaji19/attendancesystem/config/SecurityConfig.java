5eabbbbf1b409825ef370c074bac98f9286e777b
Arnab Maji <arnabmaji1999@gmail.com>
2020-11-16 12:34:15 +0530

    Add Web Security Configurations

---
+++ src/main/java/io/github/arnabmaji19/attendancesystem/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package io.github.arnabmaji19.attendancesystem.config;
+
+import io.github.arnabmaji19.attendancesystem.exceptionhandler.AuthenticationEntryPointJwt;
+import io.github.arnabmaji19.attendancesystem.filter.AuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationTokenFilter authenticationTokenFilter;
+    private final AuthenticationEntryPointJwt authenticationEntryPointJwt;
+
+    public SecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsService userDetailsService, AuthenticationTokenFilter authenticationTokenFilter, AuthenticationEntryPointJwt authenticationEntryPointJwt) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationTokenFilter = authenticationTokenFilter;
+        this.authenticationEntryPointJwt = authenticationEntryPointJwt;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/test/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(authenticationTokenFilter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling().authenticationEntryPoint(authenticationEntryPointJwt);
+    }
+}
