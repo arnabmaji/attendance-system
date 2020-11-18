d767aa253b3576041d0ef6438a1b05904561f7d7
Arnab Maji <arnabmaji1999@gmail.com>
2020-11-18 20:22:30 +0530

    Allow home route to public

--- src/main/java/io/github/arnabmaji19/attendancesystem/restcontroller/HomeController.java
+++ src/main/java/io/github/arnabmaji19/attendancesystem/restcontroller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/")
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping()
     public String home() {
         return "Attendance Management System";
     }
