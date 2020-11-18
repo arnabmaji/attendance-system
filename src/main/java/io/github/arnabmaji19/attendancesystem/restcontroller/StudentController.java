62e94d41e761f11f98a38fb18e1347461f1ae19b
Arnab Maji <arnabmaji1999@gmail.com>
2020-11-18 19:25:39 +0530

    Encode plain text password received in StudentController and Disallow same usernames for registration

--- src/main/java/io/github/arnabmaji19/attendancesystem/restcontroller/StudentController.java
+++ src/main/java/io/github/arnabmaji19/attendancesystem/restcontroller/StudentController.java
@@ -3,12 +3,14 @@ package io.github.arnabmaji19.attendancesystem.restcontroller;
 import io.github.arnabmaji19.attendancesystem.entity.StudentDetail;
 import io.github.arnabmaji19.attendancesystem.entity.User;
 import io.github.arnabmaji19.attendancesystem.model.AppRole;
+import io.github.arnabmaji19.attendancesystem.model.ResponseMessage;
 import io.github.arnabmaji19.attendancesystem.model.StudentDetails;
 import io.github.arnabmaji19.attendancesystem.service.RoleService;
 import io.github.arnabmaji19.attendancesystem.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,21 +26,27 @@ public class StudentController {
 
     private final UserService userService;
     private final RoleService roleService;
+    private final PasswordEncoder passwordEncoder;
 
-    public StudentController(UserService userService, RoleService roleService) {
+    public StudentController(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
         this.roleService = roleService;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @PostMapping("/")
     public ResponseEntity<?> add(@Valid @RequestBody StudentDetails studentDetails) {
         logger.info("Accepted User Details: " + studentDetails);
 
+        User user = userService.findByUsername(studentDetails.getUsername());
+        if (user != null)
+            return ResponseEntity.badRequest().body(new ResponseMessage("username already registered."));
+
         // Save the current user as a student
-        User user = new User(
+        user = new User(
                 studentDetails.getUsername(),
                 studentDetails.getName(),
-                studentDetails.getPassword(),
+                passwordEncoder.encode(studentDetails.getPassword()),
                 roleService.findRoleByName(AppRole.STUDENT)
         );
         StudentDetail studentDetail = new StudentDetail(user.getId(), studentDetails.getParentPhone());
